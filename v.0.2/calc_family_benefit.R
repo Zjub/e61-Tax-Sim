a614a62ce5eb1946d3270a7e0c40f3c3b64a171b
MatthewMaltman <122850716+MatthewMaltman@users.noreply.github.com>
2024-06-17 15:28:20 +1000

    Add files via upload

---
+++ v.0.2/calc_family_benefit.R
@@ -0,0 +1,135 @@
+## Calculating of the family benefit received ----
+# Last update: 25/08/2023
+# Updated by: Matt Nolan
+# Initial author: Matt Nolan
+
+## Function ----
+calc_family_benefit <- function(child_age,gross_fam_income,work_income,taxable_benefit){
+  # Note, the LITSO is not counted in the super information.
+  super_contributions = super_cont_rate*(work_income + partner_earnings)
+  ATI = gross_fam_income + super_contributions
+  net_fam_a = 0
+  net_fam_b = 0
+  net_fam = 0
+  numb_child_12 <- length(child_age[child_age < 13])
+  numb_child_19 <- length(child_age[child_age <20 & child_age > 12])
+  fam_a <- fam_a_young_pay*numb_child_12 + fam_a_old_pay*numb_child_19
+  RA <- calc_benefit_gross()[["RA"]] 
+  ES <- calc_benefit_gross()[["ES"]] 
+  
+  First_abatement <- max(min(ATI - fam_max_threshold, fam_base_threshold - fam_max_threshold), 0) 
+  Second_abatement <- max(ATI - fam_base_threshold, 0)
+  
+  if(RA_Abate == 1){
+  
+  if (Numb_dep > 0){
+    fam_a_abatement_1 <- (First_abatement*fam_max_rate + Second_abatement*fam_a_highATI_rate)/26
+    fam_a_abatement_2 <- (fam_a_highATI_rate*Second_abatement)/26
+    
+    if (fam_a + RA + ES - fam_a_abatement_1 >= 
+              fam_basic_pay*(numb_child_19+numb_child_12) - fam_a_abatement_2){
+    Fam_a_test <- "max_rate"
+    abatement_rate <-  ifelse(fam_a + RA + ES > 0 , 
+                                fam_a_abatement_1  /  (fam_a + RA + ES), 
+                               0)
+    abatement_rate <- min(abatement_rate, 1)
+    net_fam_a <- fam_a - (fam_a * abatement_rate)
+    RA <- RA - (RA * abatement_rate)
+    ES <- ES - (ES * abatement_rate)
+    }
+   else { 
+     Fam_a_test <- "base_rate"
+     abatement_rate <- fam_a_abatement_2 / fam_basic_pay*(numb_child_19+numb_child_12)
+     abatement_rate <- min(abatement_rate, 1)
+     net_fam_a <- max(fam_basic_pay*(numb_child_19+numb_child_12) - fam_a_abatement_2, 0)
+     RA <- 0 
+     ES <- 0 
+     }
+  
+   }
+    else {
+    net_fam_a = 0
+    Fam_a_test <- "NA"
+    abatement_rate <- 0
+    RA <- 0 
+    ES <- 0 
+    }
+  } else{
+    
+    if (Numb_dep > 0){
+      fam_a_abatement_1 <- (First_abatement*fam_max_rate + Second_abatement*fam_a_highATI_rate)/26
+      fam_a_abatement_2 <- fam_a_highATI_rate*Second_abatement/26
+      
+      if (fam_a  - fam_a_abatement_1 >= 
+          fam_basic_pay*(numb_child_19+numb_child_12) - fam_a_abatement_2){
+        Fam_a_test <- "max_rate"
+        abatement_rate <-  ifelse(fam_a  > 0 , 
+                                  fam_a_abatement_1  /  (fam_a), 
+                                  0)
+        abatement_rate <- min(abatement_rate, 1)
+        net_fam_a <- max(fam_a - (fam_a * abatement_rate),0)
+        }
+      else { 
+        Fam_a_test <- "base_rate"
+        abatement_rate <- fam_a_abatement_2 / fam_basic_pay*(numb_child_19+numb_child_12)
+        abatement_rate <- min(abatement_rate, 1)
+        net_fam_a <- min(fam_basic_pay*(numb_child_19+numb_child_12) - fam_a_abatement_2, 0)
+        RA <- 0 
+        ES <- 0 
+      }
+      
+    } else {
+      net_fam_a = 0
+      Fam_a_test <- "NA"
+      abatement_rate <- 0
+      RA <- 0 
+      ES <- 0 
+    }
+    
+    
+    
+  }
+  
+  
+  # Add Family Benefit B
+  FTB_B_eligibility <- 0
+  if(length(child_age[child_age < 13]) > 1){
+    FTB_B_eligibility <- 1 
+  }
+  if(length(child_age[child_age < 19]) > 1 & partnered == 0){
+    FTB_B_eligibility <- 1 
+  }
+  
+  
+  numb_child_5 <- length(child_age[child_age < 5])
+  numb_child_18 <- length(child_age[child_age < 19 & child_age > 4])
+  
+  fam_b <- fcase(numb_child_5 > 0, fam_b_young_pay,
+                 numb_child_18 > 0, fam_b_old_pay,
+                 default = 0)
+  
+  highest_hh_income <- max(partner_earnings *(1+super_cont_rate) , work_income *(1+super_cont_rate) + taxable_benefit)
+  lowest_hh_income <- min(partner_earnings *(1+super_cont_rate) , work_income *(1+super_cont_rate) + taxable_benefit)
+  
+  fam_b_abatement <- 0.2*max(lowest_hh_income - fam_be_low_thresh, 0) / 26
+  
+  
+  
+  if (partnered == 0){
+    net_fam_b = ifelse(ATI > fam_b_threshold,0,fam_b) # The documentation appears to suggest no abatement, just a cliff face!
+  }   else{
+    net_fam_b = ifelse(highest_hh_income  > fam_b_threshold, 0 , max(fam_b - fam_b_abatement, 0))
+  }
+  net_fam_b <- net_fam_b * FTB_B_eligibility
+  
+  fam_a_ann_supp <- ifelse(ATI < fam_ftba_eoy_incthreshold, fam_ftba_eoy_supp * Numb_dep, 0) / 26
+  net_fam_a <- ifelse(net_fam_a > 0, net_fam_a + fam_a_ann_supp, 0)
+  
+  net_fam_b <- ifelse(net_fam_b > 0, (fam_ftbb_eoy_supp / 26) + net_fam_b, 0)
+  
+  
+  net_fam = net_fam_a + net_fam_b
+  return(list(net_fam = net_fam,gross_fam = fam_a + fam_b, net_fam_a = net_fam_a, net_fam_b = net_fam_b,
+              Fam_a_test = Fam_a_test, abatement_rate = abatement_rate, RA = RA, 
+              ES = ES))
+}
---
+++ v.0.2/calc_income_tax.R
@@ -0,0 +1,77 @@
+## Calculating of the gross benefit payment ----
+# Last update: 25/08/2023
+# Updated by: Matt Nolan
+# Initial author: Matt Nolan
+
+# Function for calculating income tax ----
+# Includes Low Income Tax Credit, Low and Middle Income Tax Credit, Temporary Budget Repair Levy, and Beneficiary Tax offset.
+
+calc_income_tax <- function(gross_income,work_income) {
+  
+  # Set up the income tax scale
+  tax_brackets <- c(0, tax_free, tax_threshold_1, tax_threshold_2, tax_threshold_3)
+  tax_scale <- c(0, tax_rate_1, tax_rate_2, tax_rate_3, tax_rate_4)
+  
+  tax_nc <- 0
+  tempinc <- gross_income
+  for (i in length(tax_brackets):2) {
+    if (gross_income > tax_brackets[i]) {
+      tax_nc <- tax_nc + (tempinc - tax_brackets[i]) * tax_scale[i]
+      tempinc <- tax_brackets[i]
+    }
+  }
+
+  LITO_credit <- 0
+  if (LITO_on == 1){
+    LITO_credit <- max(0,LITO_gross - max(0,(gross_income - LITO_thresh_1)*LITO_rate_1) - max(0,(gross_income - LITO_thresh_2)*LITO_rate_2))
+  }
+
+  LMITO_credit <- 0
+  if (LMITO_on == 1){
+    LMITO_credit <- ifelse(gross_income <= LMITO_thresh_1,LMITO_gross,ifelse(gross_income <= LMITO_thresh_2,LMITO_gross + (gross_income-LMITO_thresh_1)*LMITO_inc_rate,ifelse(gross_income <- LMITO_thresh_3,LMITO_max,max(0,LMITO_max - (gross_income - LMITO_thresh_3)*LMITO_dec_rate))))
+  }
+
+  BRL <- 0
+  if (BRL_on == 1){
+    BRL <- max(0,(gross_income - BRL_thresh)*BRL_rate)
+  }
+
+  BTO_credit <- 0
+  benefit_paid <- calc_benefit_abated(work_income,partner_earnings)[["net_benefit"]]*26
+  if (benefit_paid > BTO_thresh){
+    BTO_credit <- BTO_rate*(benefit_paid - BTO_thresh)
+  }
+  
+  
+  tax <- max(0,(tax_nc + BRL - LITO_credit - LMITO_credit - BTO_credit))
+  
+  medicarelevy <- 0 
+ 
+if(partnered == 0){
+    if(gross_income >= medi_LI_threshold_S & gross_income < medi_PI_threshold_S){
+      
+      medicarelevy <-  (gross_income - medi_LI_threshold_S) * 0.1
+          } 
+  
+  if( gross_income > medi_PI_threshold_S){
+    
+    medicarelevy <-  gross_income * 0.02
+  } 
+}
+  
+  if(partnered == 1){
+    if(gross_income > medi_LI_threshold_P + Numb_dep * medi_dep_LI_threshold & 
+       gross_income < medi_PI_threshold_P + Numb_dep * medi_dep_PI_threshold ){
+      
+      medicarelevy <-  (gross_income - medi_LI_threshold_P - Numb_dep * medi_dep_LI_threshold) * 0.1
+    } 
+    
+    if( gross_income >= medi_PI_threshold_P + Numb_dep * medi_dep_PI_threshold){
+      
+      medicarelevy <-  gross_income * 0.02
+    } 
+  }
+  
+  
+  return(list(tax = tax, medicarelevy = medicarelevy))
+}
